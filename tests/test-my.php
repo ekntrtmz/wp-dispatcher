2b1862144dcdc738d50f7908473f645fc96df0d9
ekntrtmz <hi@ekntrtmz.com>
2021-04-18 19:59:23 +0200

    Add wp testing suite

---
+++ tests/test-my.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class MyTest
+ *
+ * @package Wp_Dispatcher
+ */
+
+/**
+ * Sample test case.
+ */
+class MyTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_my() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
