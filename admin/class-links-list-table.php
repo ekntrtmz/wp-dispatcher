5466a97170dd9bbfc14cdaa80c2c890048b473f8
Joan Rodas <joan@sirvelia.com>
2023-05-09 22:16:01 +0200

    Add translatable strings + Shortcode exp

--- admin/class-links-list-table.php
+++ admin/class-links-list-table.php
@@ -1,9 +1,8 @@
 <?php
 
-if(!class_exists('Custom_WP_List_Table')){
+if (!class_exists('Custom_WP_List_Table')) {
 
-  require_once plugin_dir_path( dirname( __FILE__ ) ) . 'libraries/class-wp-list-table.php';
-
+  require_once plugin_dir_path(dirname(__FILE__)) . 'libraries/class-wp-list-table.php';
 }
 
 
@@ -13,68 +12,70 @@ if(!class_exists('Custom_WP_List_Table')){
  * in a WordPress-like Admin Table with row actions to 
  * perform user meta opeations
  */
-class Links_List_Table extends Custom_WP_List_Table {
+class Links_List_Table extends Custom_WP_List_Table
+{
 
-  public function search_box( $text, $input_id ){} // Remove search box
+  public function search_box($text, $input_id)
+  {
+  } // Remove search box
   //protected function pagination( $which ){}        // Remove pagination
   //protected function display_tablenav( $which ){}  // Remove navigation
 
 
-  function __construct(){
-      global $status, $page;
-              
-      //Set parent defaults
-      parent::__construct( array(
-          'singular'  => 'link',     //singular name of the listed records
-          'plural'    => 'links',    //plural name of the listed records
-          'ajax'      => false        //does this table support ajax?
-      ) );
-      
+  function __construct()
+  {
+    global $status, $page;
+
+    //Set parent defaults
+    parent::__construct(array(
+      'singular'  => 'link',     //singular name of the listed records
+      'plural'    => 'links',    //plural name of the listed records
+      'ajax'      => false        //does this table support ajax?
+    ));
   }
 
-  function column_default($item, $column_name){
+  function column_default($item, $column_name)
+  {
 
-    
+
     $has_expired = false;
     $expiration_date = new DateTime($item["expires"]);
 
-    if($expiration_date > new DateTime()) {
+    if ($expiration_date > new DateTime()) {
       $remaining_time = $expiration_date->diff(new DateTime());
       $expires_in = $remaining_time->format("%a days, %h hours, %i minutes, %s seconds");
     } else {
-      $has_expired=true;
-      $expires_in="0";
+      $has_expired = true;
+      $expires_in = "0";
     }
 
     // Expensive filename fetch | uploads<>links: 1-n-relationship
     global $wpdb;
     $uploads_table_name = $wpdb->prefix . 'dispatcher_uploads';
-    $filename = $wpdb->get_var( 
+    $filename = $wpdb->get_var(
       "SELECT filename FROM {$uploads_table_name} WHERE id = {$item['upload_id']}"
-     );
+    );
 
     $url = get_site_url() . '/?resolve=' . $item["hash_id"];
 
-    switch($column_name){        
-        case 'filename':
-            return $filename;
-        case 'expires':
-          return $expires_in;
-        case 'link':
-          if($has_expired){
-            return "Link has expired.";
-          }
-          else {
-            return '<a href="' . $url . '">' . $url . '</a>';
-          }
-        default:
-            return print_r($item,true); //Show the whole array for troubleshooting purposes
+    switch ($column_name) {
+      case 'filename':
+        return $filename;
+      case 'expires':
+        return $expires_in;
+      case 'link':
+        if ($has_expired) {
+          return "Link has expired.";
+        } else {
+          return '<a href="' . $url . '">' . $url . '</a>';
+        }
+      default:
+        return print_r($item, true); //Show the whole array for troubleshooting purposes
     }
+  }
 
-}
 
-
-    /** ************************************************************************
+  /** ************************************************************************
    * REQUIRED! This method dictates the table's columns and titles. This should
    * return an array where the key is the column slug (and class) and the value 
    * is the column's title text. If you need a checkbox for bulk actions, refer
@@ -87,165 +88,168 @@ class Links_List_Table extends Custom_WP_List_Table {
    * @see WP_List_Table::::single_row_columns()
    * @return array An associative array containing column information: 'slugs'=>'Visible Titles'
    **************************************************************************/
-  function get_columns(){
-      $columns = array(
-        'filename'     => 'Dispatched File',
-        'expires'  => 'Expires in',
-        'link' =>  'Direct Download Link'
-      );
-      return $columns;
+  function get_columns()
+  {
+    $columns = [
+      'filename'     => __('Dispatched File', 'wp-dispatcher'),
+      'expires'  => __('Expires in', 'wp-dispatcher'),
+      'link' =>  __('Direct Download Link',  'wp-dispatcher')
+    ];
+    return $columns;
   }
 
-  function column_cb($item){
+  function column_cb($item)
+  {
     return '<span class="dashicons dashicons-download"></span>';
-}
+  }
 
   /** ************************************************************************
-     * REQUIRED! This is where you prepare your data for display. This method will
-     * usually be used to query the database, sort and filter the data, and generally
-     * get it ready to be displayed. At a minimum, we should set $this->items and
-     * $this->set_pagination_args(), although the following properties and methods
-     * are frequently interacted with here...
+   * REQUIRED! This is where you prepare your data for display. This method will
+   * usually be used to query the database, sort and filter the data, and generally
+   * get it ready to be displayed. At a minimum, we should set $this->items and
+   * $this->set_pagination_args(), although the following properties and methods
+   * are frequently interacted with here...
+   * 
+   * @global WPDB $wpdb
+   * @uses $this->_column_headers
+   * @uses $this->items
+   * @uses $this->get_columns()
+   * @uses $this->get_sortable_columns()
+   * @uses $this->get_pagenum()
+   * @uses $this->set_pagination_args()
+   **************************************************************************/
+  function prepare_items()
+  {
+
+    global $wpdb; //This is used only if making any database queries
+
+    /**
+     * First, lets decide how many records per page to show
+     */
+    $per_page = 5;
+
+
+    /**
+     * REQUIRED. Now we need to define our column headers. This includes a complete
+     * array of columns to be displayed (slugs & titles), a list of columns
+     * to keep hidden, and a list of columns that are sortable. Each of these
+     * can be defined in another method (as we've done here) before being
+     * used to build the value for our _column_headers property.
+     */
+    $columns = $this->get_columns();
+    $hidden = array();
+    //$sortable = $this->get_sortable_columns();
+
+
+    /**
+     * REQUIRED. Finally, we build an array to be used by the class for column 
+     * headers. The $this->_column_headers property takes an array which contains
+     * 3 other arrays. One for all columns, one for hidden columns, and one
+     * for sortable columns.
+     */
+    $this->_column_headers = array($columns, $hidden);
+
+
+    /**
+     * Optional. You can handle your bulk actions however you see fit. In this
+     * case, we'll handle them within our package just to keep things clean.
+     */
+    //$this->process_bulk_action();
+
+
+    /**
+     * Instead of querying a database, we're going to fetch the example data
+     * property we created for use in this plugin. This makes this example 
+     * package slightly different than one you might build on your own. In 
+     * this example, we'll be using array manipulation to sort and paginate 
+     * our data. In a real-world implementation, you will probably want to 
+     * use sort and pagination data to build a custom query instead, as you'll
+     * be able to use your precisely-queried data immediately.
+     */
+    //$data = $this->example_data;
+
+
+    /**
+     * This checks for sorting input and sorts the data in our array accordingly.
      * 
-     * @global WPDB $wpdb
-     * @uses $this->_column_headers
-     * @uses $this->items
-     * @uses $this->get_columns()
-     * @uses $this->get_sortable_columns()
-     * @uses $this->get_pagenum()
-     * @uses $this->set_pagination_args()
-     **************************************************************************/
-    function prepare_items() {
+     * In a real-world situation involving a database, you would probably want 
+     * to handle sorting by passing the 'orderby' and 'order' values directly 
+     * to a custom query. The returned data will be pre-sorted, and this array
+     * sorting technique would be unnecessary.
+     */
+    // function usort_reorder($a,$b){
+    //     $orderby = (!empty($_REQUEST['orderby'])) ? $_REQUEST['orderby'] : 'title'; //If no sort, default to title
+    //     $order = (!empty($_REQUEST['order'])) ? $_REQUEST['order'] : 'asc'; //If no order, default to asc
+    //     $result = strcmp($a[$orderby], $b[$orderby]); //Determine sort order
+    //     return ($order==='asc') ? $result : -$result; //Send final sort direction to usort
+    // }
+    //usort($data, 'usort_reorder');
 
-      global $wpdb; //This is used only if making any database queries
 
-      /**
-       * First, lets decide how many records per page to show
-       */
-      $per_page = 5;
-      
-      
-      /**
-       * REQUIRED. Now we need to define our column headers. This includes a complete
-       * array of columns to be displayed (slugs & titles), a list of columns
-       * to keep hidden, and a list of columns that are sortable. Each of these
-       * can be defined in another method (as we've done here) before being
-       * used to build the value for our _column_headers property.
-       */
-      $columns = $this->get_columns();
-      $hidden = array();
-      //$sortable = $this->get_sortable_columns();
-      
-      
-      /**
-       * REQUIRED. Finally, we build an array to be used by the class for column 
-       * headers. The $this->_column_headers property takes an array which contains
-       * 3 other arrays. One for all columns, one for hidden columns, and one
-       * for sortable columns.
-       */
-      $this->_column_headers = array($columns, $hidden);
-      
-      
-      /**
-       * Optional. You can handle your bulk actions however you see fit. In this
-       * case, we'll handle them within our package just to keep things clean.
-       */
-      //$this->process_bulk_action();
-      
-      
-      /**
-       * Instead of querying a database, we're going to fetch the example data
-       * property we created for use in this plugin. This makes this example 
-       * package slightly different than one you might build on your own. In 
-       * this example, we'll be using array manipulation to sort and paginate 
-       * our data. In a real-world implementation, you will probably want to 
-       * use sort and pagination data to build a custom query instead, as you'll
-       * be able to use your precisely-queried data immediately.
-       */
-      //$data = $this->example_data;
-              
-      
-      /**
-       * This checks for sorting input and sorts the data in our array accordingly.
-       * 
-       * In a real-world situation involving a database, you would probably want 
-       * to handle sorting by passing the 'orderby' and 'order' values directly 
-       * to a custom query. The returned data will be pre-sorted, and this array
-       * sorting technique would be unnecessary.
-       */
-      // function usort_reorder($a,$b){
-      //     $orderby = (!empty($_REQUEST['orderby'])) ? $_REQUEST['orderby'] : 'title'; //If no sort, default to title
-      //     $order = (!empty($_REQUEST['order'])) ? $_REQUEST['order'] : 'asc'; //If no order, default to asc
-      //     $result = strcmp($a[$orderby], $b[$orderby]); //Determine sort order
-      //     return ($order==='asc') ? $result : -$result; //Send final sort direction to usort
-      // }
-      //usort($data, 'usort_reorder');
-      
-      
-      /***********************************************************************
-       * ---------------------------------------------------------------------
-       * vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-       * 
-       * In a real-world situation, this is where you would place your query.
-       *
-       * For information on making queries in WordPress, see this Codex entry:
-       * http://codex.wordpress.org/Class_Reference/wpdb
-       * 
-       * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-       * ---------------------------------------------------------------------
-       **********************************************************************/
+    /***********************************************************************
+     * ---------------------------------------------------------------------
+     * vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+     * 
+     * In a real-world situation, this is where you would place your query.
+     *
+     * For information on making queries in WordPress, see this Codex entry:
+     * http://codex.wordpress.org/Class_Reference/wpdb
+     * 
+     * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+     * ---------------------------------------------------------------------
+     **********************************************************************/
 
-      $table_name = $wpdb->prefix . 'dispatcher_links';
+    $table_name = $wpdb->prefix . 'dispatcher_links';
 
-      $data = $wpdb->get_results( "SELECT * FROM {$table_name} ORDER BY id DESC" , ARRAY_A);
-      
-      
-              
-      /**
-       * REQUIRED for pagination. Let's figure out what page the user is currently 
-       * looking at. We'll need this later, so you should always include it in 
-       * your own package classes.
-       */
-      $current_page = $this->get_pagenum();
-      
-      /**
-       * REQUIRED for pagination. Let's check how many items are in our data array. 
-       * In real-world use, this would be the total number of items in your database, 
-       * without filtering. We'll need this later, so you should always include it 
-       * in your own package classes.
-       */
-      $total_items = count($data);
-      
-      
-      /**
-       * The WP_List_Table class does not handle pagination for us, so we need
-       * to ensure that the data is trimmed to only the current page. We can use
-       * array_slice() to 
-       */
-      $data = array_slice($data,(($current_page-1)*$per_page),$per_page);
-      
-      
-      
-      /**
-       * REQUIRED. Now we can add our *sorted* data to the items property, where 
-       * it can be used by the rest of the class.
-       */
-      $this->items = $data;
-      
-      
-      /**
-       * REQUIRED. We also have to register our pagination options & calculations.
-       */
-      $this->set_pagination_args( array(
-          'total_items' => $total_items,                  //WE have to calculate the total number of items
-          'per_page'    => $per_page,                     //WE have to determine how many items to show on a page
-          'total_pages' => ceil($total_items/$per_page)   //WE have to calculate the total number of pages
-      ) );
-}
+    $data = $wpdb->get_results("SELECT * FROM {$table_name} ORDER BY id DESC", ARRAY_A);
+
+
+
+    /**
+     * REQUIRED for pagination. Let's figure out what page the user is currently 
+     * looking at. We'll need this later, so you should always include it in 
+     * your own package classes.
+     */
+    $current_page = $this->get_pagenum();
+
+    /**
+     * REQUIRED for pagination. Let's check how many items are in our data array. 
+     * In real-world use, this would be the total number of items in your database, 
+     * without filtering. We'll need this later, so you should always include it 
+     * in your own package classes.
+     */
+    $total_items = count($data);
+
+
+    /**
+     * The WP_List_Table class does not handle pagination for us, so we need
+     * to ensure that the data is trimmed to only the current page. We can use
+     * array_slice() to 
+     */
+    $data = array_slice($data, (($current_page - 1) * $per_page), $per_page);
+
+
+
+    /**
+     * REQUIRED. Now we can add our *sorted* data to the items property, where 
+     * it can be used by the rest of the class.
+     */
+    $this->items = $data;
+
+
+    /**
+     * REQUIRED. We also have to register our pagination options & calculations.
+     */
+    $this->set_pagination_args(array(
+      'total_items' => $total_items,                  //WE have to calculate the total number of items
+      'per_page'    => $per_page,                     //WE have to determine how many items to show on a page
+      'total_pages' => ceil($total_items / $per_page)   //WE have to calculate the total number of pages
+    ));
+  }
 
   /** Text displayed when no customer data is available */
-  public function no_items() {
-    _e( 'No links available.', 'wp_dispatcher' );
+  public function no_items()
+  {
+    _e('No links available.', 'wp_dispatcher');
   }
-  
 }
--- admin/class-wp-dispatcher-add-new-upload.php
+++ admin/class-wp-dispatcher-add-new-upload.php
@@ -15,7 +15,8 @@
  * Class WordPress_Plugin_Template_Settings
  *
  */
-class Wp_Dispatcher_Add_New_Upload {
+class Wp_Dispatcher_Add_New_Upload
+{
 
 	/**
 	 * The ID of this plugin.
@@ -42,109 +43,104 @@ class Wp_Dispatcher_Add_New_Upload {
 	 * @param      string    $plugin_name       The name of this plugin.
 	 * @param      string    $version    The version of this plugin.
 	 */
-	public function __construct( $plugin_name, $version ) {
+	public function __construct($plugin_name, $version)
+	{
 
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
-
 	}
 
 	/**
 	 * This function introduces the theme options into the 'Appearance' menu and into a top-level
 	 * 'WPPB Demo' menu.
 	 */
-	public function setup_plugin_sub_menu() {
+	public function setup_plugin_sub_menu()
+	{
 
-    add_submenu_page(
-      'wp_dispatcher',
-      'Add New',
-      'Add New',
-      'manage_options',
-      'wp_dispatcher_new',
-      array( $this, 'render_new_upload_page_content'),				// The name of the function to call when rendering this menu's page
+		add_submenu_page(
+			'wp_dispatcher',
+			__('Add New', 'wp-dispatcher'),
+			__('Add New', 'wp-dispatcher'),
+			'manage_options',
+			'wp_dispatcher_new',
+			array($this, 'render_new_upload_page_content'),				// The name of the function to call when rendering this menu's page
 
-  );
-
+		);
 	}
 
 	/**
 	 * Renders a simple page to display for the theme menu defined above.
 	 */
-	public function render_new_upload_page_content(  ) {
+	public function render_new_upload_page_content()
+	{
 
-		?>
+?>
 		<!-- Create a header in the default WordPress 'wrap' container -->
 		<div class="wrap">
 
-			<h2><?php _e( 'WP Dispatcher', 'wp-dispatcher' ); ?></h2>
-      <?php settings_errors(); ?>
-				<h2>Add new file</h2>
+			<h2><?php _e('WP Dispatcher', 'wp-dispatcher'); ?></h2>
+			<?php settings_errors(); ?>
+			<h2><?php _e('Add new file', 'wp-dispatcher'); ?></h2>
 
-				<!-- Form to handle the upload - The enctype value here is very important -->
-				<form action="<?php echo get_site_url() ?>/wp-admin/admin-post.php"  method="post" enctype="multipart/form-data">
-					<input type="hidden" name="action" value="process_upload">
-          <input type='file' id='file_upload' name='file_upload'></input>
-          <?php submit_button('Upload') ?>
-      </form>
-   		
-	</div><!-- /.wrap -->
-	<?php
-  }
-  
+			<!-- Form to handle the upload - The enctype value here is very important -->
+			<form action="<?php admin_url('admin-post.php') ?>" method="post" enctype="multipart/form-data">
+				<input type="hidden" name="action" value="process_upload">
+				<input type='file' id='file_upload' name='file_upload'></input>
+				<?php submit_button(__('Upload', 'wp-dispatcher')) ?>
+			</form>
 
-  public function wp_dispatcher_process_upload(){
+		</div><!-- /.wrap -->
+<?php
+	}
+
+
+	public function wp_dispatcher_process_upload()
+	{
 		// First check if the file appears on the _FILES array
-		
-		if($_FILES['file_upload']["error"] == 4) {
-			wp_redirect( get_admin_url() . 'admin.php?page=wp_dispatcher_new&upload=empty');
+		if ($_FILES['file_upload']["error"] == 4) {
+			wp_safe_redirect(get_admin_url() . 'admin.php?page=wp_dispatcher_new&upload=empty');
 			exit();
 		}
-		else {
-			if(isset($_FILES['file_upload'])){
 
-				$filename = sanitize_file_name($_FILES['file_upload']['name']);
-				//$filename = $_FILES['file_upload']['name'];
+		if (isset($_FILES['file_upload'])) {
 
-        //$ext = pathinfo($_FILES['file_upload']['name'], PATHINFO_EXTENSION);
-        //$uuid = uniqid();
+			$filename = sanitize_file_name($_FILES['file_upload']['name']);
+			//$filename = $_FILES['file_upload']['name'];
 
-				$destination = WP_CONTENT_DIR . '/uploads/wp-dispatcher/' . $filename;
-				
-				// Check if file already exists
-				if (file_exists($destination))
-				{
-					wp_redirect( get_admin_url() . 'admin.php?page=wp_dispatcher_new&upload=duplicate');
-					exit();
-				}
-				else {
+			//$ext = pathinfo($_FILES['file_upload']['name'], PATHINFO_EXTENSION);
+			//$uuid = uniqid();
 
-					$uploaded = move_uploaded_file( $_FILES['file_upload']['tmp_name'], $destination );
+			$destination = WP_CONTENT_DIR . '/uploads/wp-dispatcher/' . $filename;
 
-					// Error checking using WP functions
-					if(is_wp_error($uploaded)){
-							echo "Error uploading file: " . $uploaded->get_error_message();
-					} else {
-	
-						// Insert to data base
-						global $wpdb;
-						$table_name = $wpdb->prefix . 'dispatcher_uploads';
-	
-						$wpdb->insert( 
-							$table_name, 
-							array( 
-								'date' => current_time( 'mysql' ), 
-								'count' => 0,
-								'author' => wp_get_current_user()->user_login,
-								'filename' => $filename, 
-							) 
-						);
-	
-						wp_redirect( get_admin_url() . 'admin.php?page=wp_dispatcher&upload=success');
-						exit();
-					}
-
-				}
+			// Check if file already exists
+			if (file_exists($destination)) {
+				wp_safe_redirect(get_admin_url() . 'admin.php?page=wp_dispatcher_new&upload=duplicate');
+				exit();
 			}
+
+
+			$uploaded = move_uploaded_file($_FILES['file_upload']['tmp_name'], $destination);
+
+			if ($uploaded) {
+				// Insert to database
+				global $wpdb;
+				$table_name = $wpdb->prefix . 'dispatcher_uploads';
+
+				$wpdb->insert(
+					$table_name,
+					array(
+						'date' => current_time('mysql'),
+						'count' => 0,
+						'author' => wp_get_current_user()->user_login,
+						'filename' => $filename,
+					)
+				);
+
+				wp_safe_redirect(get_admin_url() . 'admin.php?page=wp_dispatcher&upload=success');
+				exit();
+			}
+
+			echo __('Error uploading file.', 'wp-dispatcher');
 		}
 	}
 }
